251df6fe9dde5e4a454c2555efe8d7c4b5ff1582
Xavier Duncan <xduncan@pm.me>
2018-09-24 15:44:53 -0400

    add stock card element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginScreen from './login-screen';
+import MarketScreen from './market-screen';
 
-ReactDOM.render(<LoginScreen />, document.getElementById('root'));
+ReactDOM.render(<MarketScreen />, document.getElementById('root'));
