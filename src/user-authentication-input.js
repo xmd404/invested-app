91fe0599eab4364091f7ca49358be8fbd2a6f1b5
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-09-25 14:13:54 -0400

    corrected use of value variable in user authentication input component

--- src/user-authentication-input.js
+++ src/user-authentication-input.js
@@ -5,7 +5,7 @@ let UserAuthenticationInput = (props) => {
         <input 
         onChange={ (event) => {
             let value = event.target.value;
-            props.stateFunction(event.target.value);
+            props.stateFunction(value);
         } }
         value={props.stateName}
         type={props.type} className={props.className} placeholder={props.placeHolder}>
