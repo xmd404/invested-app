b93a001cc7ce7966470af3df411807001520a1a4
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-09-28 07:31:56 -0400

    changed component back to original code

--- client/src/user-name.js
+++ client/src/user-name.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 let UserName = ()=>
     <div>
-        <h3>User Name </h3>
+        <h3>User Name</h3>
         <h4>Cash: </h4>
         <h4>Portfolio Value: </h4>
     </div>
