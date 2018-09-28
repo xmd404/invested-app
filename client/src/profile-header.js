3b312ba1cf2f1e3c693b78feddfd08f9222b402a
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-09-28 07:14:02 -0400

    profile header is flex direction column as standard, then media query changes to row at 768px

--- client/src/profile-header.js
+++ client/src/profile-header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 let ProfileHeader = (props) =>
-    <div>
-        <p>User Name: {props.userName}</p>
-        <p>Cash: {props.userCash}</p>
-        <p>Portfolio Value: {props.userPortfolioValue}</p>
+    <div className="profile-header">
+        <h3>User Name: {props.userName}</h3>
+        <h3>Cash: {props.userCash}</h3>
+        <h3>Portfolio Value: {props.userPortfolioValue}</h3>
     </div>
 
 
